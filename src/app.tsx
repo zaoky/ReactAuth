d868c88c0a3b67498f0eb228b0604fda0f06c686
zaoky <proxtos@gmail.com>
2016-11-09 18:06:26 -0600

    modify import

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { Header } from './Components/common/';
+import { Header } from './Components/common';
 
 const {Component} = React;
 
